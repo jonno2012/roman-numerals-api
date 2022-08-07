8ea7b187fac3dde9afd7794367ac3effad812dc0
Jon Hill <jonathan.hill@impactreporting.co.uk>
2022-08-07 19:11:31 +0100

    Composer and migrations

---
+++ database/migrations/2022_08_07_090545_create_conversions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConversionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('conversions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('integer')->unique();
+            $table->integer('count')->index()->default(0)->nullable();
+            $table->string('numeral')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('conversions');
+    }
+}
